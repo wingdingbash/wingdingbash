eae2d4f3bd06a801de0a82d9e075ef174a617dcd
Jatin <jassharma000@gmail.com>
2021-06-29 22:38:39 +0530

    Update urls.py

--- wingding/urls.py
+++ wingding/urls.py
@@ -23,6 +23,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls'))
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
