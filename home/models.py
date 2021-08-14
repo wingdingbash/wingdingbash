8053404637adce6db7cb27719c3fcf53041dcafd
Jatin <jassharma000@gmail.com>
2021-08-14 22:55:07 +0530

    .
    .

--- home/models.py
+++ home/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class city(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='pics')
+    image = models.ImageField(upload_to='')
     desc = models.TextField()
     offer= models.BooleanField(default=False)
